48b6dc53d1cb5e0fb561787c68fd16827e4b0206
daihiep <mrbjkut0@gmail.com>
2017-06-02 11:07:25 +0700

    add source code

---
+++ index.php
@@ -0,0 +1,243 @@
+<!DOCTYPE html>
+<html lang="">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title></title>
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+		<link rel="stylesheet" href="common/css/bootstrap.min.css" />
+		<link rel="stylesheet" href="common/css/font-awesome.min.css" />
+		<!-- style css -->
+		<link rel="stylesheet" href="css/style-above.css" />
+		<!-- <link rel="stylesheet" href="css/style-below.css" /> -->
+
+		<!-- jquery -->
+		<script src="common/js/jquery-1.9.1.min.js"></script>
+		<!-- Slider -->
+		<link rel="stylesheet" href="plugins/owl-carousel/owl.carousel.css" />
+		<link rel="stylesheet" href="plugins/owl-carousel/owl.theme.css" />
+		<script src="plugins/owl-carousel/owl.carousel.min.js"></script>
+
+		<script>
+	    $(document).ready(function () {
+
+	        $("#owl-quandt").owlCarousel({
+
+	            autoPlay: true, //Set AutoPlay to 3 seconds
+	            navigation: true, // Show next and prev buttons
+	            pagination : false,
+	            slideSpeed: 5000,
+	            paginationSpeed: 3000,
+	            stopOnHover : true,
+	            navigationText : ['<i class="fa fa-chevron-left" aria-hidden="true"></i>', '<i class="fa fa-chevron-right" aria-hidden="true"></i>'],
+	            singleItem: true
+	        });
+
+	    });
+	</script>
+
+		
+	</head>
+	<body>
+		<div class="get-started">
+			<div class="title">
+				<h1>get the website you deserve</h1>
+			</div>
+			<div class="container-fluid">
+				<div class="row">	
+					<div class="img-screen">
+						<img src="images/screenshots3.png" class="img-responsive" alt="Image">
+					</div>
+					<div class="btn-getstarted">
+						<button type="button" class="btn btn-primary">get started<i class="line"></i><i class="fa fa-angle-double-right" aria-hidden="true"></i></button>
+					</div>
+					<div class="quality">
+						<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 left-content">
+							<img src="images/laptopmac.png" class="img-responsive" alt="Image">
+						</div>
+						<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+							<ul class="exp">
+								<li> <img src="images/icon-dola.png"> affordable</li>
+								<li> <img src="images/icon-made.png"> tailor made</li>
+								<li> <img src="images/icon-star.png"> top notch</li>
+							</ul>
+						</div>
+						<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+							<ul class="skill">
+								<li><i class="fa fa-hand-o-right" aria-hidden="true"></i> 100% transparence</li>
+								<li><i class="fa fa-hand-o-right" aria-hidden="true"></i> Skilled, experenced team</li>
+								<li><i class="fa fa-hand-o-right" aria-hidden="true"></i> Focus and results</li>
+								<li><i class="fa fa-hand-o-right" aria-hidden="true"></i> On time dilivery</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="design">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-xs-6 col-sm-6 col-md-3 col-lg-3 padding-none">
+						<div class="cell cell-11">
+							<p>web design</p>
+						</div>
+					</div>
+					<div class="col-xs-6 col-sm-6 col-md-3 col-lg-3 padding-none">
+						<div class="cell">
+							<img src="images/icon-spen.png" class="img-responsive" alt="Image">
+						</div>
+					</div>
+					<div class="col-xs-6 col-sm-6 col-md-3 col-lg-3 padding-none">
+						<div class="cell">
+							<img src="images/icon-shopping.png" class="img-responsive" alt="Image">
+						</div>
+					</div>
+					<div class="col-xs-6 col-sm-6 col-md-3 col-lg-3 padding-none">
+						<div class="cell cell-14">
+							<p>E-commerce Website Development</p>
+						</div>
+					</div>
+					<div class="col-xs-6 col-sm-6 col-md-3 col-lg-3 padding-none">
+						<div class="cell cell-21">
+							<img src="images/icon-wp.png" class="img-responsive" alt="Image">
+						</div>
+					</div>
+					<div class="col-xs-6 col-sm-6 col-md-3 col-lg-3 padding-none">
+						<div class="cell cell-22">
+							<p>Wordpress Development</p>
+						</div>
+					</div>
+					<div class="col-xs-6 col-sm-6 col-md-3 col-lg-3 padding-none">
+						<div class="cell cell-23">
+							<p>Laravel  Development</p>
+						</div>
+					</div>
+					<div class="col-xs-6 col-sm-6 col-md-3 col-lg-3 padding-none">
+						<div class="cell cell-24">
+							<img src="images/icon-laravel.png" class="img-responsive" alt="Image">
+						</div>
+					</div>
+					<div class="col-xs-6 col-sm-6 col-md-3 col-lg-3 padding-none">
+						<div class="cell cell-31">
+							<p>MVC based  Framework  Development</p>
+						</div>
+					</div>
+					<div class="col-xs-6 col-sm-6 col-md-3 col-lg-3 padding-none">
+						<div class="cell">
+							<img src="images/icon-cake.png" class="img-responsive" alt="Image">
+						</div>
+					</div>
+					<div class="col-xs-6 col-sm-6 col-md-3 col-lg-3 padding-none">
+						<div class="cell">
+							<img src="images/icon-other.png" class="img-responsive" alt="Image">
+						</div>
+					</div>
+					<div class="col-xs-6 col-sm-6 col-md-3 col-lg-3 padding-none">
+						<div class="cell cell-34">
+							<p>Other custom code development</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="work">
+			<div class="title">
+				<h1>our works</h1>
+			</div>
+			<div id="owl-quandt">
+	            <div class="item">
+	                <img src="images/work-item.png" class="img-responsive" alt="Image">
+	            </div>
+	            <div class="item">
+	                <img src="images/work-item.png" class="img-responsive" alt="Image">
+	            </div>
+	            <div class="item">
+	                <img src="images/work-item.png" class="img-responsive" alt="Image">
+	            </div>
+			</div>
+		</div>
+
+		<div class="testimonials">
+			<div class="title">
+				<h1>testimonials</h1>
+			</div>
+			<div class="content">
+				<p>"text"</p>
+			</div>
+		</div>
+
+		<div class="work-start">
+			<div class="btn-getstarted">
+				<button type="button" class="btn btn-primary">get started<i class="line"></i><i class="fa fa-angle-double-right" aria-hidden="true"></i></button>
+			</div>
+		</div>
+
+		<div class="contact">
+			<div class="title">
+				<h1>contact us</h1>
+			</div>
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+						<div class="about">
+							<h4>If you have a project you would like to discuss, <br>get in touch with us.</h4>
+							<p>Morbi interdum mollis sapien. Sed ac risus. Phasellus lacinia, magna a ullamcorper laoreet, lectus arcu pulvinar risus, vitae facilisis libero dolor a purus. </p>
+							<p><strong>OneUp Studio</strong><br>
+							24 Rue de Saint Martin<br>
+							Paris 1297<br>
+							France</p>
+							<p><span class="phone">+11 (0) 123 456 78</span><br>
+							<a href="mailto:hello@emailaddress.com">bonjour@oneup.com</a></p>
+						</div>
+					</div>
+					<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+						<div class="form-contact">
+							<form action="sendmail.php" method="POST" class="form-horizontal" role="form">
+								<div class="form-group">
+								    <label for="name" class="col-sm-2 control-label">Name:</label>
+								    <div class="col-sm-10">
+								      <input type="text" class="form-control" name="name" id="name" placeholder="Name" required="required"><span class="required">*</span>
+								    </div>
+								</div>
+								<div class="form-group">
+								    <label for="email" class="col-sm-2 control-label">Email:</label>
+								    <div class="col-sm-10">
+								      <input type="email" class="form-control" name="email" id="email" placeholder="Email" required="required"><span class="required">*</span>
+								    </div>
+								</div>
+								<div class="form-group">
+								    <label for="message" class="col-sm-2 control-label">Message:</label>
+								    <div class="col-sm-10">
+								      <textarea type="text" class="form-control" name="message" id="message" placeholder="Message" rows="10" required="required"></textarea><span class="required">*</span>
+								    </div>
+								</div>
+									
+								<div class="form-group">
+									<div class="col-sm-10 col-sm-offset-2">
+										<button type="submit" class="btn btn-send">send</button>
+									</div>
+								</div>
+							</form><!--end form-->
+						</div>
+					</div>
+				</div>
+				
+			</div>
+		</div>
+
+		<div class="footer">
+			<div class="logo">
+				<a href="#"><img src="images/logo.png" class="img-responsive" alt="Image"></a>
+			</div>
+		</div>
+
+
+
+
+		<!-- <script src="common/js/jquery.min.js"></script> -->
+		<script src="common/js/bootstrap.min.js"></script>
+		<script src="plugins/jqueryEasing.js"></script>
+		
+	</body>
+</html>
---
+++ sendmail.php
@@ -0,0 +1,41 @@
+﻿<?php
+    //goi thu vien
+    include('mail/class.smtp.php');
+    include "mail/class.phpmailer.php"; 
+    $nFrom = "Vinsofts.com";    //mail duoc gui tu dau, thuong de ten cong ty ban
+    $mFrom = 'mrbjkut0@gmail.com';  //dia chi email cua ban 
+    $mPass = 'loveyouandloveme';       //mat khau email cua ban
+    $nTo = 'HoangHiep'; //Ten nguoi nhan
+    $mTo = 'mr.bjn102@gmail.com';   //dia chi nhan mail
+    $mail             = new PHPMailer();
+    $body             = $_REQUEST['message'];   // Noi dung email
+    $title = $_REQUEST['name'].' - '.$_REQUEST['email'];   //Tieu de gui mail
+    $mail->IsSMTP();             
+    $mail->CharSet  = "utf-8";
+    $mail->SMTPDebug  = 0;   // enables SMTP debug information (for testing)
+    $mail->SMTPAuth   = true;    // enable SMTP authentication
+    $mail->SMTPSecure = "ssl";   // sets the prefix to the servier
+    $mail->Host       = "smtp.gmail.com";    // sever gui mail.
+    $mail->Port       = 465;         // cong gui mail de nguyen
+    // xong phan cau hinh bat dau phan gui mail
+    $mail->Username   = $mFrom;  // khai bao dia chi email
+    $mail->Password   = $mPass;              // khai bao mat khau
+    $mail->SetFrom($mFrom, $nFrom);
+    $mail->AddReplyTo('mrbjkut0@gmail.com', 'Vinsofts.com'); //khi nguoi dung phan hoi se duoc gui den email nay
+    $mail->Subject    = $title;// tieu de email 
+    $mail->MsgHTML($body);// noi dung chinh cua mail se nam o day.
+    $mail->AddAddress($mTo, $nTo);
+    // thuc thi lenh gui mail 
+    if(!$mail->Send()) {
+        echo "<script>";
+        echo "alert('Không thể gửi mail, vui lòng xem lại!');";
+        echo "window.location.assign('http://new-waffle.yez.vn/vinsofts/')";
+        echo "</script>";
+         
+    } else {
+         echo "<script>";
+        echo "alert('Mail của bạn đã được gửi!');";
+        echo "window.location.assign('http://new-waffle.yez.vn/vinsofts/')";
+        echo "</script>";
+    }
+?>
